808d92d88fb462fe97e494631a62dec839fd5b33
farooqiusman <ufben10@gmail.com>
2021-10-24 04:50:04 +0000

    Set up bot for working

---
+++ bot.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const {Client, Intents} = require("discord.js")
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES,
+    ],
+});
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)})
+
+
+client.on("message", msg => {
+    if (msg.content == "ping"){
+        msg.reply("pong")
+    }
+
+})
+
+client.login(process.env.DISCORD_TOKEN)
+
